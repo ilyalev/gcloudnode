8756ba58113dc7bf819c3083492bc8cb8e10fe36
Левиков Илья Николаевич <Ilya.Levikov@chtpz.ru>
2019-12-16 10:08:21 +0500

    2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(80)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
